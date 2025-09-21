262cbb7c0f39187889089fe768340c4ca209222e
Chiriku2013 <suzukichiriku@gmail.com>
2025-09-21 20:39:37 +0700

    Create config.js

---
+++ chatapp/config.js
@@ -0,0 +1,10 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBolPBpLhLtRcxPdtYONK_wkSrcdKwtbr4",
+  authDomain: "chat-app-202f3.firebaseapp.com",
+  databaseURL: "https://chat-app-202f3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-app-202f3",
+  storageBucket: "chat-app-202f3.appspot.com",
+  messagingSenderId: "914610018913",
+  appId: "1:914610018913:android:d2c1b1e47341f16a992820"
+};
