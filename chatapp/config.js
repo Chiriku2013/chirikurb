05ecd2804858d5265ec48a2b5c1eb2f788b7845d
Chiriku2013 <suzukichiriku@gmail.com>
2025-09-21 21:46:02 +0700

    Update config.js

--- chatapp/config.js
+++ chatapp/config.js
@@ -1,9 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: "AIzaSyCP1SjtCN5v2gCbwB7UMa69KsgxAOAZdPs",
+const firebaseConfig = {
+  apiKey: "AIzaSyBolPBpLhLtRcxPdtYONK_wkSrcdKwtbr4",
   authDomain: "chat-app-web-a3f29.firebaseapp.com",
+  databaseURL: "https://chat-app-web-a3f29-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "chat-app-web-a3f29",
-  storageBucket: "chat-app-web-a3f29.firebasestorage.app",
+  storageBucket: "chat-app-web-a3f29.appspot.com",
   messagingSenderId: "110363862630",
   appId: "1:110363862630:web:d5c1f27ec438ff98f62fa7",
   measurementId: "G-XF2SRTP5H2"
